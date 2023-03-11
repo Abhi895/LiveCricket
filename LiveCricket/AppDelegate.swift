ef9e9a81c8d088c68718bb5777b9cc9839acacd3
Abhi Reddy <reddy.abhi9876@gmail.com>
2023-03-11 12:49:51 +0000

    Initial Commit

---
+++ LiveCricket/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  LiveCricket
+//
+//  Created by Abhi Reddy on 11/03/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ LiveCricket/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LiveCricket
+//
+//  Created by Abhi Reddy on 11/03/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
